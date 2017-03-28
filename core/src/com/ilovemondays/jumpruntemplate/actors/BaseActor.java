a638f8a69bd96a1663864ec4e6bc5488f8e71574
Matthias Dietrich <mat.dietrich1985@gmail.com>
2017-03-28 13:55:11 +0200

    Initial commit

---
+++ core/src/com/ilovemondays/jumpruntemplate/actors/BaseActor.java
@@ -0,0 +1,7 @@
+package com.ilovemondays.jumpruntemplate.actors;
+
+/**
+ * Created by Matthias on 21.10.2016.
+ */
+public class BaseActor {
+}
---
+++ desktop/src/com/ilovemondays/jumpruntemplate/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ilovemondays.jumpruntemplate.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ilovemondays.jumpruntemplate.JumpRunTemplate;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpRunTemplate(), config);
+	}
+}
