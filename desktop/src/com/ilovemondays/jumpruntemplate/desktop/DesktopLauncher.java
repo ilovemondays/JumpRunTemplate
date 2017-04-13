775b46318f4ce10753fc1d842b4274165216c5ac
Matthias Dietrich <mat.dietrich1985@gmail.com>
2017-04-14 01:43:52 +0200

    Testing Music & Backgrounds

--- desktop/src/com/ilovemondays/jumpruntemplate/desktop/DesktopLauncher.java
+++ desktop/src/com/ilovemondays/jumpruntemplate/desktop/DesktopLauncher.java
@@ -7,9 +7,11 @@ import com.ilovemondays.jumpruntemplate.JumpRunTemplate;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		/*
 		config.width = 1650;
 		config.height = 1050;
 		config.fullscreen = true;
+		*/
 		new LwjglApplication(new JumpRunTemplate(), config);
 	}
 }
